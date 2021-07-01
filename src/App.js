226ad237ba93ff0b8994d080052ad9cbb8cc8efd
Amrit Joshi <amrit5joshi@gmail.com>
2021-07-01 23:22:48 +0545

    commit 13th chapter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Greet from './Component/Greet.js';
 import Navbar from './Navbar';
+import Home from './Home';
 function App() {
+
+  
   return (
     <div className="App">
       <Navbar/>
       <div className="content">
-      Ma ta amrit ho hai
+     <Home/>
       </div>
       
-      <Greet />
+     
+
     </div>
   );
 }
