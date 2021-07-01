26dee853405ee9e5113dd16ac2ba0a3def496ade
Amrit Joshi <amrit5joshi@gmail.com>
2021-07-01 16:12:28 +0545

    initial commit

---
+++ src/Component/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Greet = () => {
+    const a="amrit joshi is my name hai ta";
+    return ( 
+        <div>
+            <h1>Hello {a} </h1>
+        </div>
+        
+     );
+}
+ 
+export default Greet;
