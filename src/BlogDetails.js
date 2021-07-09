2df68705bd52df3f4789de2395fcaed38ce3fb44
Amrit Joshi <amrit5joshi@gmail.com>
2021-07-09 20:54:41 +0545

    Project Completed

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog,error,isPending} = useFetch('http://localhost:8000/blogs/'+id)
+    
+    const history=useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/'+blog.id, {
+            method: 'DELETE'
+          }).then(() => {
+            // history.go(-1);
+            history.push('/');
+          })
+    }
+
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading.</div> }
@@ -16,6 +28,7 @@ const BlogDetails = () => {
                          <div>
                              {blog.body}
                          </div>
+                         <button onClick={handleClick}>delete</button>
                 </article>
             )
             }
