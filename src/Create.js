3978318680a0053aad9e9d7a4340805c45099634
Amrit Joshi <amrit5joshi@gmail.com>
2021-07-09 19:12:09 +0545

    commit 29th chapter

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Create = () => {
+    const [title,setTitle] = useState('hello');
+    const [body,setBody] = useState('');
+    const [author,setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog ={ title, body, author};
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs',{
+            method: 'POST',
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("New Blog added!!!");
+            setIsPending(false);
+        })
+    }
+
     return ( 
         <div className="create">
-            <h2> Add a new blog </h2>
+            <form  onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange= { (e) => setTitle(e.target.value)}
+                />
+                
+                <label>Blog Body:</label>
+                <textarea 
+                required
+                value={body}
+                onChange = { (e)=>{
+                    setBody(e.target.value)
+                }}
+                >
+                </textarea>
+                
+                <select
+                value={author}
+                onChange={(e)=>{
+                    setAuthor(e.target.value)
+                }}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {
+                    !isPending
+                    &&
+                    <button>Add Blog</button>
+                }
+                {
+                    isPending
+                    &&
+                    <button disabled>Adding Blog...</button>
+                }
+                
+            </form>
         </div>
      );
 }
