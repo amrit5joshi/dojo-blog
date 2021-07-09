481b82a838ebd0bff9c9128584c93c340b90fadc
Amrit Joshi <amrit5joshi@gmail.com>
2021-07-09 17:08:16 +0545

    commit 26th chapter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+
+
 function App() {
 
   
   return (
-    <div className="App">
+    <Router>
+        <div className="App">
       <Navbar/>
       <div className="content">
-     <Home/>
+      <Switch>
+         
+         <Route exact path="/">
+            <Home/>
+         </Route>
+
+         <Route path="/create">
+            <Create/>
+         </Route>
+           
+
+         <Route path="/blogs/:id">
+            <BlogDetails/>
+         </Route>
+           
+      </Switch>
       </div>
     </div>
+    </Router>
+    
   );
 }
 
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const BlogList = ({blogs}) => {
     return ( 
         <div className="blog-list">
             {blogs.map(blog => (
               <div className="blog-preview" key={blog.id} >
+              <Link to={`/blogs/${blog.id}`}>
               <h3>{ blog.title }</h3>
               <h2>Written by { blog.author }</h2>
-              <p>{blog.body}</p>
+              </Link>
               </div>
             ))}
         </div>
